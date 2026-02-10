24f67b02945b67a014a5adff533afdb5c406d7f1
john-fried <srimaulani1977@gmail.com>
2026-02-10 20:45:20 +0700

    compiler: add simple documentation

--- compiler.c
+++ compiler.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <errno.h>
 
+/* instruction table */
 const Inst_Def inst_table[COUNT_INST] = {
     [INST_HALT]  = { .type = INST_HALT,  .name = "halt", .operand = 0 },
     [INST_PUSH]  = { .type = INST_PUSH,  .name = "push", .operand = 1,
@@ -48,12 +49,13 @@ ST_FUNC void compiler_error(const char *fmt, ...)
 	fflush(stderr);
 	error_count++;
 
-	// skip until newline (one line, one error)
+	/* skip until newline (one line, one error -- prevent garbage error from one line) */
 	while (*cursor != '\n' && *cursor != '\0') consume();
 }
 
 /********************** Lexer *************************/
 
+/* consume -- move cursor to the next character */
 ST_FUNC void consume(void)
 {
 	if (*cursor == '\0')
@@ -68,11 +70,13 @@ ST_FUNC void consume(void)
 	cursor++;
 }
 
+/* next -- take the next token from cursor */
 ST_FUNC Token next(void)
 {
 	while (*cursor != '\0') {
 		if (isspace((unsigned char)*cursor) || *cursor == ',')
 			consume();
+		/* skip comment until newline */
 		else if (*cursor == ';')
 			while (*cursor != '\n' && *cursor != '\0') consume();
 		else
@@ -86,6 +90,7 @@ ST_FUNC Token next(void)
 		return t;
 	}
 
+	/* digit? OR ('-'? AND digit(after '-')? ) */
 	if (isdigit((unsigned char)*cursor) || (*cursor == '-' &&
 	    isdigit((unsigned char)*(cursor + 1)))) {
 		t.type = TOK_INT;
@@ -95,20 +100,25 @@ ST_FUNC Token next(void)
 		t.length = end - cursor;
 		if (errno == ERANGE)
 			compiler_error("Integrer out of range");
+		/* consume integrer */
 		for (unsigned int i=0;i < t.length;i++)
 			consume();
 		return t;
 	}
 
+	/* decide whether is identifier or register.
+	   check a-Z
+	*/
 	if (isalpha((unsigned char)*cursor)) {
 		char *start = cursor;
+		/* check a-Z or 1-10 or '_' */
 		while (isalnum((unsigned char)*cursor) || *cursor == '_')
 			consume();
 
 		t.length = cursor - start;
 		t.type = TOK_IDENT;
 
-		// check if register
+		/* check if register */
 		if (start[0] == 'r' && isdigit(start[1])) {
 			t.type = TOK_REGISTER;
 			t.value = atoi(start + 1);
@@ -117,14 +127,18 @@ ST_FUNC Token next(void)
 		return t;
 	}
 
+	/* no valid token */
 	compiler_error("Illegal character '%c' in program", *cursor);
 	consume();
 	return t;
 }
 
+/* peek -- take the next n token from cursor
+   without moving the cursor (unlike next())
+*/
 ST_FUNC Token peek(int n)
 {
-	// save "before" data
+	/* save "before" data */
 	char *saved_cursor = cursor;
 	unsigned int saved_line = line;
 	unsigned int saved_col = col;
@@ -143,11 +157,20 @@ ST_FUNC Token peek(int n)
 
 /*********************** Parser *****************************/
 
+/* take the next token from cursor and store it into parser lookahead */
 ST_FUNC void parser_advance(Parser *p)
 {
 	p->lookahead = next();
 }
 
+/* parse instruction, the compiler core -- using the Instruction Set Architecture (ISA)
+   it will do the following thing:
+   - search if instruction is in the table,
+   - check the operand count && operand types to prevent from conflict cases like:
+     - "add": INST_ADD, operand: {reg,reg}
+     - "add": INST_ADDI, operand: {reg,imm} (immidiate value)
+   - and then write the mnemonic opcode, and the operand opcode if all procedure passed
+*/
 ST_FUNC void parse_inst(Parser *p)
 {
 	unsigned int table = 0;
@@ -155,9 +178,9 @@ ST_FUNC void parse_inst(Parser *p)
 	const Inst_Def *inst = NULL;
 
 search_table:
-	// search from instruction table
+	/* search from instruction table */
 	for (; table < COUNT_INST; table++) {
-		// @check name
+		/* @check name */
 		if (strncmp(inst_table[table].name, mnemonic.start, mnemonic.length) == 0 &&
 			strlen(inst_table[table].name) == mnemonic.length) {
 			inst = &inst_table[table];
@@ -171,24 +194,27 @@ search_table:
 		return;
 	}
 
-	// @validate operand
+	/* validate operand */
 	int match = 1;
 	Token t;
 
 	for (int i = 0; i < inst->operand; i++) {
 		t = peek(i + 1);
+		/* check if the operand is suitable with the next token */
 		if (inst->operand_type[i] == OPR_REG && t.type != TOK_REGISTER) match = 0;
 		if (inst->operand_type[i] == OPR_IMM && t.type != TOK_INT) match = 0;
 	}
 
+	/* if no match increment table to continue searching again from the next table */
 	if (!match) {
 		inst = NULL;
 		table++;
 		goto search_table;
 	}
 
+	/* write opcode */
 	uint8_t opcode = (uint8_t)inst->type;
-	parser_advance(p); // skip mnemonic
+	parser_advance(p); /* skip mnemonic */
 	fwrite(&opcode, 1, 1, p->out);
 
 	for (int i = 0; i < inst->operand; i++) {
@@ -223,18 +249,20 @@ int pocol_compile_file(char *path, char *out)
 	p.lookahead = next();
 	/* Compiling process */
 	while (p.lookahead.type != TOK_EOF) {
+		/* only parse instruction if the token.type is identifier */
 		if (p.lookahead.type == TOK_IDENT) {
 			parse_inst(&p);
 			continue;
 		}
 
-		parser_advance(&p);
+		parser_advance(&p); /* next token (if token != identifier) */
 	}
 
 	munmap(source, st.st_size);
 	close(fd);
 	fclose(p.out);
 
+	/* move tempfile to cwd output */
 	if (rename(tempfile, out) < 0) {
 		source_file = tempfile;
 		compiler_error("failed to move to `%s`", out);
@@ -242,7 +270,7 @@ int pocol_compile_file(char *path, char *out)
 		goto error;
 	}
 
-	// mark executable
+	/* mark executable */
 	chmod(out, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP
 		| S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
 	return 0;
